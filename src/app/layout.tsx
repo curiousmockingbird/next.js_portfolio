5da95f6aae68341e7ab17d0bc2707f2a213dc7df
Harold Mesa <harold.mesa@icloud.com>
2024-04-08 19:06:42 -0500

    Add vercel/analytics and vercel/speedInsights.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import TranStackProvider from './components/providers/TanStackProvider'
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,9 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}><TranStackProvider>
+      <body className={inter.className}>
+        <TranStackProvider>
         {children}
         </TranStackProvider>
+        <Analytics />
+        <SpeedInsights />
         </body>
     </html>
   )
