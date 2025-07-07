8997baa3256a8278ef1c8ebbfda1c62cd77829db
Harold Mesa <101518420+curiousmockingbird@users.noreply.github.com>
2025-07-07 23:14:15 +0000

    Add marquee text effect

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,7 +32,7 @@ const config: Config = {
         },
       },
       animation: {
-        marquee: 'marquee 20s linear infinite',
+        marquee: 'marquee 40s linear infinite',
       },
     },
   },
