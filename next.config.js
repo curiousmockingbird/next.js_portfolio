9ccab57ec88c7ef7956056717546f9baf4c8d3d4
Harold Mesa <101518420+curiousmockingbird@users.noreply.github.com>
2025-08-04 12:48:41 -0500

    Allow Spotify image host

--- next.config.js
+++ next.config.js
@@ -9,10 +9,15 @@ const nextConfig = {
         {
           protocol: "https",
           hostname: "res.cloudinary.com", // Cloudinary domain moved here
-        },{
+        },
+        {
         protocol: 'https',
         hostname: 'static01.nyt.com',   // NY Times image CDN
         pathname: '/images/**',        // optional: limit to the images folder
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
       },
       ],
   },
